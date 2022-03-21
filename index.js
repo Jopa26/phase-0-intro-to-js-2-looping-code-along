1a29ca63f9fd58fbe44066167652f00bebe5d8e6
Luis <jqluis2011@gmail.com>
2022-03-20 20:42:48 -0700

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event){
+
+    let greetings = [];
+    for(let i=.0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+    }
+        return greetings;
+           
+}
+
+function countDown(){
+    let countDown = 10;
+
+    while (countDown >= 0   ){
+        console.log(countDown--);
+    }
+
+}
